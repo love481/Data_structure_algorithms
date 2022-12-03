dc7abf85c0efd205e5879bd578c0d8cfb8373308
love481 <075bei016.love@pcampus.edu.np>
2022-12-03 16:51:54 +0545

    dynamic_programming

--- dynamic_programming/minimizing_coin.cpp
+++ dynamic_programming/minimizing_coin.cpp
@@ -1,9 +1,12 @@
 #include<bits/stdc++.h>
 using namespace std;
-void minimize_coin(vector<int>coins,int amt,int *min_coins)
+int minimize_coin(vector<int>coins,int amt,int *min_coins)
 {
     int soln[amt+1];
     soln[0]=0;
+    int total_no_sol[amt+1];
+    memset(total_no_sol,0,sizeof total_no_sol);
+    total_no_sol[0]=1;
     for (int i=1;i<=amt;i++)
     {
         soln[i]=INT_MAX;
@@ -14,8 +17,11 @@ void minimize_coin(vector<int>coins,int amt,int *min_coins)
                 soln[i]=soln[i-coin]+1;
                 min_coins[i]=coin;
             }   
+            if(i-coin>-1)
+                total_no_sol[i]+=total_no_sol[i-coin];
         }
     }
+    return total_no_sol[amt];
 
 }
 int main(void)
@@ -23,11 +29,14 @@ int main(void)
     vector<int>coins{1,3,4};
     int total_sum=10;
     int min_coins[total_sum+1];
-    minimize_coin(coins,total_sum,min_coins);
+    int max_soln=minimize_coin(coins,total_sum,min_coins);
+    cout<<"optimal_solutions is: ";
     while(total_sum>0)
     {
         cout<<min_coins[total_sum]<<" ";
         total_sum-=min_coins[total_sum];
     }
+    cout<<endl;
+    cout<<"max_no_of_solutions: "<<max_soln;
     cout<<endl;
 }
