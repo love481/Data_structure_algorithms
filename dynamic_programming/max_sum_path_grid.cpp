f427eed52b44fe537064c1127637353c5d0745dd
love481 <075bei016.love@pcampus.edu.np>
2022-12-05 18:23:39 +0545

    added_path_sum

--- dynamic_programming/max_sum_path_grid.cpp
+++ dynamic_programming/max_sum_path_grid.cpp
@@ -3,25 +3,32 @@ using namespace std;
 #define N 5
 int max_path(vector<vector<int>>&matrix)
 {
-    vector<vector<int>>mat(matrix.size()+1,vector<int>(matrix[0].size()+1));
-    for (int i=1;i<=matrix.size();i++)
+    for (int i=1;i<matrix.size();i++)
     {
-        for(int j=1;j<=matrix[0].size();j++)
+        matrix[i][0]+=matrix[i-1][0];
+    }
+     for (int i=1;i<matrix[0].size();i++)
+    {
+        matrix[0][i]+=matrix[0][i-1];
+    }
+    for (int i=1;i<matrix.size();i++)
+    {
+        for(int j=1;j<matrix[0].size();j++)
         {
-            mat[i][j]=max(mat[i][j-1],mat[i-1][j])+matrix[i-1][j-1];            
+            matrix[i][j]=max(matrix[i][j-1],matrix[i-1][j])+matrix[i][j];            
         }
     }
-    return mat[matrix.size()][matrix[0].size()];
+    return matrix[matrix.size()-1][matrix[0].size()-1];
 }
 int main(void)
 {
     vector<vector<int>>matrix
     {
-        {1, 2, 7, 1, 4},
-        {3, 1, 2, 4, 5},
-        {1, 2, 3, 6, 4},
-        {2, -4, -1, 1, 4},
-        {1, 1, 2, 5, 5},
+        {3, 7, 9, 2, 7},
+        {9, 8, 3, 5, 5},
+        {1, 7, 9, 8, 6},
+        {3, 8, 6, 4, 9},
+        {6, 3, 9, 7, 8}
     };
     cout<<max_path(matrix)<<endl;
 
